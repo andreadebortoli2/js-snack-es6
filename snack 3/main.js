fd257b8bfa67dd6191c9d3e1d9cc77b810c51890
andreadebortoli2 <debortoliandrea88@gmail.com>
2024-03-05 15:26:42 +0100

    add snack 3

--- snack 3/main.js
+++ snack 3/main.js
@@ -27,3 +27,55 @@ Es:
 
 Crea un nuovo array con la lista dei mammiferi.
 */
+
+const animals = [
+    {
+        nome: 'leone',
+        famiglia: 'felidi',
+        classe: 'mammiferi',
+    },
+    {
+        nome: 'orso',
+        famiglia: 'ursidi',
+        classe: 'mammiferi',
+    },
+    {
+        nome: 'squalo',
+        famiglia: 'condritti',
+        classe: 'pesci',
+    },
+    {
+        nome: 'rana',
+        famiglia: 'anuri',
+        classe: 'anfibi',
+    },
+    {
+        nome: 'tartaruga',
+        famiglia: 'sauropsidi',
+        classe: 'rettili',
+    },
+    {
+        nome: 'elefante',
+        famiglia: 'elefantidi',
+        classe: 'mammiferi',
+    },
+    {
+        nome: 'alligatore',
+        famiglia: 'alligatoridi',
+        classe: 'rettili',
+    },
+    {
+        nome: 'cavallo',
+        famiglia: 'equidi',
+        classe: 'mammiferi',
+    },
+    {
+        nome: 'gufo',
+        famiglia: 'strigidi',
+        classe: 'uccelli',
+    },
+];
+
+const mammals = animals.filter(animal => animal.classe === 'mammiferi');
+
+console.log(mammals);
