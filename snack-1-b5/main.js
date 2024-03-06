c72cc54a92dc52827e794470658f1d7bbb998a41
andreadebortoli2 <debortoliandrea88@gmail.com>
2024-03-06 11:57:20 +0100

    add snack-1-b5

---
+++ snack-1-b5/main.js
@@ -0,0 +1,67 @@
+// console.log('test');
+
+/*
+Crea un array di 10 oggetti che rappresentano una zucchina, indicando per ognuna varietà, peso e lunghezza.
+Calcola quanto pesano tutte le zucchine.
+*/
+
+const zucchine = [
+    {
+        varieta: 'zucca romanesca',
+        peso: 3,
+        lunghezza: 15,
+    },
+    {
+        varieta: 'zucca trombetta',
+        peso: 2,
+        lunghezza: 21,
+    },
+    {
+        varieta: 'zucca costata',
+        peso: 7,
+        lunghezza: 8,
+    },
+    {
+        varieta: 'zucca crookneck',
+        peso: 1,
+        lunghezza: 28,
+    },
+    {
+        varieta: 'zucca zucchini rampicante',
+        peso: 5,
+        lunghezza: 19,
+    },
+    {
+        varieta: 'zucca romanesca',
+        peso: 3,
+        lunghezza: 15,
+    },
+    {
+        varieta: 'zucca trombetta',
+        peso: 2,
+        lunghezza: 21,
+    },
+    {
+        varieta: 'zucca costata',
+        peso: 7,
+        lunghezza: 8,
+    },
+    {
+        varieta: 'zucca crookneck',
+        peso: 1,
+        lunghezza: 28,
+    },
+    {
+        varieta: 'zucca zucchini rampicante',
+        peso: 5,
+        lunghezza: 19,
+    },
+];
+
+let weight = 0;
+
+zucchine.forEach((zucchina) => {
+    weight = weight + zucchina.peso
+});
+
+console.log(weight);
